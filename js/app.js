447545c487c557366c28a9bea5ae7cd7b393f273
Zuri Hunter <zuri.hunter@thehumangeo.com>
2016-09-09 10:58:46 -0400

    Have data coming back working on getting data to show on html

--- js/app.js
+++ js/app.js
@@ -1,10 +1,29 @@
 console.log("I AM WORKING");
 
-var url="https://api.meetup.com/Women-Who-Code-DC/events/228457104/rsvps";
+$(document).ready(function () {
+  var url="https://api.meetup.com/Women-Who-Code-DC/events/228457104/rsvps";
 
-$.ajax({
-  url: url,
-  data: data,
-  success: success,
-  dataType: dataType
+  $.ajax({
+    url: url,
+    method: 'GET',
+    dataType: 'jsonp',
+    success: function(response){
+      console.log("THE RESPONSE", response);
+    },
+    error: function(error) {
+      console.log("THE ERROR", error);
+
+    }
+  });
+
+  ///addEvent Information
+  function adEventInfo (data){
+    
+    var addEvent = {
+      eName: eName,
+      eLocation: eLocation,
+      eTime: eTime,
+      eRsvps: eRsvps
+    }
+  }
 });
