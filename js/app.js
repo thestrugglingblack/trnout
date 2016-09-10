fbf1bbd60cd836829bde02a3386101b00cff9e10
Zuri Hunter <zuri.hunter@thehumangeo.com>
2016-09-10 18:04:33 -0400

    Generic ajax call

--- js/app.js
+++ js/app.js
@@ -1,40 +1,70 @@
 console.log("I AM WORKING");
 
-$(document).ready(function () {
-
-  var urlEvent="https://api.meetup.com/Women-Who-Code-DC/events/228457104";
-
+function fetchData(url,successMethod) {
   $.ajax({
     method: 'GET',
-    url: urlEvent,
+    url: url,
     dataType: 'jsonp',
-    success: function (response) {
-      addEventInfo(response);
-    },
-    error: function (error) {
+    success: successMethod,
+    error: function (error){
       console.error(error);
     }
   });
-
-});
-
-
-function addEventInfo(response) {
-
-  eventInfo = {
-    name: response.data.name,
-    location: response.data.venue.address_1,
-    time: response.data.time,
-    rsvps: response.data.yes_rsvp_count,
-    info: response.data.description
-  }
-  console.log("THE RESPONSE", response);
-
-  console.log("EVENT INFO", eventInfo);
-
-  $eventName = $('.event-name-label')
-  $eventName.append('<p>' + eventInfo.name + '</p>');
-
-
 }
-addEventInfo();
+
+function itWork(response) {
+  console.log("It work", response);
+}
+
+
+fetchData("https://api.meetup.com/Women-Who-Code-DC/events/228457104", itWork);
+
+
+// $(document).ready(function () {
+
+  // var fetchData = function(url) {
+  // var urlEvent="https://api.meetup.com/Women-Who-Code-DC/events/228457104";
+  //
+  //   $.ajax({
+  //     method: 'GET',
+  //     url: urlEvent,
+  //     dataType: 'jsonp',
+  //     success: function (response) {
+  //       addEventInfo(response);
+  //     },
+  //     error: function (error) {
+  //       console.error(error);
+  //     }
+  //   });
+  // };
+
+
+// });
+
+
+// function addEventInfo(response) {
+
+  // fetchData(urlEvent);
+
+  // console.log(fetchData);
+//   eventInfo = {
+//     name: response.data.name,
+//     location: response.data.venue.address_1,
+//     time: response.data.time,
+//     rsvps: response.data.yes_rsvp_count,
+//     info: response.data.description
+//   }
+//   console.log("THE RESPONSE", response);
+//
+//   console.log("EVENT INFO", eventInfo);
+//
+//   $eventName = $('.event-name-label')
+//   $eventName.append('<p>' + eventInfo.name + '</p>');
+//
+//
+// }
+// addEventInfo();
+//
+// function addRsvps(response) {
+//
+// }
